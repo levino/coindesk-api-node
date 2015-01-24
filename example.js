dea7dac55ccdd6cd9a144ed18df8467e1bf60666
Levin Keller <levin@coyno.com>
2015-01-24 17:12:14 +0100

    Getting multiple currencies.

--- example.js
+++ example.js
@@ -1,6 +1,21 @@
 var coindeskapi = require('./index.js');
 var CoinDeskAPI = new coindeskapi();
-
-CoinDeskAPI.getPrices('2010-07-17', '2011-07-17', 'USD', function(err, result) {
+/*
+CoinDeskAPI.getPricesForSingleCurrency('2010-07-17', '2010-07-19', 'USD', function(err, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(result);
+    }
+});
+CoinDeskAPI.supportedCurrencies(function(err,result){
     console.log(result);
+});
+*/
+CoinDeskAPI.getPricesForMultipleCurrencies('2010-07-17', '2011-07-19', ['USD','EUR'], function(err, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(result);
+    }
 });
--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 var request = require('request'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    async = require("async");
 
-var CoinDeskAPI = function CoinDeskAPI(options) {
-    this.options = options || { }
+var CoinDeskAPI = function CoinDeskAPI() {
+};
+
+var baseUrl = 'http://api.coindesk.com/v1/bpi/';
+
+/**
+ * Returning a list of all currently "supported" (attention, querys will fail for some currencies
+ * from this list as an array of three letter strings.
+ * @param callback
+ */
+CoinDeskAPI.prototype.supportedCurrencies = function(callback) {
+    var url = baseUrl + 'supported-currencies.json';
+    request.get({uri: url}, function (err, response, body) {
+        if (err) {
+            callback(err, null);
+        }
+        if (body) {
+            console.log(body);
+            var currencies = JSON.parse(body);
+            var currencyCodeArray = _.map(currencies, function(item) {
+                return item['currency'];
+            });
+            callback(null, currencyCodeArray);
+        }
+    });
 };
 /**
  * Calling the callback on an array of objects of the format: {time, rate} where rate is a float and time is in milliseconds.
@@ -11,7 +35,7 @@ var CoinDeskAPI = function CoinDeskAPI(options) {
  * @param currency
  * @param callback
  */
-CoinDeskAPI.prototype.getPrices = function(from, to, currency, callback) {
+CoinDeskAPI.prototype.getPricesForSingleCurrency = function(from, to, currency, callback) {
     if (from > to) {
         callback(new Error('To date is before from date.'), []);
         return;
@@ -21,20 +45,89 @@ CoinDeskAPI.prototype.getPrices = function(from, to, currency, callback) {
             from = '2010-07-17';
     }
     var url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=' + from + '&end=' + to + '&currency=' + currency;
+   // console.log(url);
     request.get({uri: url}, function (err, response, body) {
             if (err) {
-                callback(err, {});
+                callback(err, null);
             }
             if (body) {
+                console.log(body);
                 var ratesValues = JSON.parse(body),
                     exchangeRatesCurrency = ratesValues.bpi;
 
                 var resultArray = _.map(exchangeRatesCurrency, function (key, item) {
                     return {time: (Date.parse(item)), rate: key};
                 });
-                callback({}, resultArray);
+                callback(null, resultArray);
             }
-        });
+    });
 };
+
+CoinDeskAPI.prototype.getPricesForMultipleCurrencies = function(from, to, currencies, callback) {
+    var self = this;
+    var ratesWithTimes = {};
+    var allRates = {};
+    async.each(currencies,
+        // 2nd param is the function that each item is passed to
+        function(currency, callback){
+            // Call an asynchronous function, often a save() to DB
+            self.getPricesForSingleCurrency(from, to, currency, function (err, result){
+                if (err) {
+                    callback(err);
+                } else {
+                    ratesWithTimes[currency] = result;
+                    callback();
+                }
+            });
+        },
+        // 3rd param is the function to call when everything's done
+        function(err){
+            if (err) {
+                callback(err, null);
+            }
+            _.each(ratesWithTimes, function(rates, currency) {
+                rates.forEach(function(timeratepair) {
+                    var newEntry = {};
+                    newEntry[currency] = timeratepair.rate;
+                    if (allRates[timeratepair.time]) {
+                        allRates[timeratepair.time].push(newEntry);
+                    } else {
+                        allRates[timeratepair.time] = [newEntry];
+                    }
+                })
+            });
+            var allRatesNice = {};
+            _.each(allRates, function(rates, time) {
+                var ratesNice = {};
+                _.each(rates, function(currencyratepair) {
+                    _.each(currencyratepair, function (rate, currency) {
+                        ratesNice[currency] = rate;
+                    });
+                });
+                allRatesNice[time] = ratesNice;
+            });
+            callback(null, allRatesNice);
+        }
+    );
+};
+
+/**
+ * Currenlty broken. Coindesk does not support all currency that are returned by the according query.
+ * @param from
+ * @param to
+ * @param currencies
+ * @param callback
+ */
+/*
+CoinDeskAPI.prototype.getPricesForAllCurrencies = function(from, to, currencies, callback) {
+  var self = this;
+    self.supportedCurrencies(function(err, currencies) {
+        if (err) {
+            callback(err, null);
+        }
+        self.getPricesForMultipleCurrencies(from, to, currencies, callback);
+    })
+};
+*/
 
 module.exports = CoinDeskAPI;
